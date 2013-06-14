318052bd60e38c0cba5a5cda6ca5c1e24b8ee4b8
unknown <audionautics@gmail.com>
2013-06-14 13:03:06 -0400

    Moved html/css logic to seperate madule.

--- validator.py
+++ validator.py
@@ -10,9 +10,10 @@ submittal.
 
 import os
 import sys
+import css
 import time
-import urllib
-import urllib2
+import html
+
 import inspect
 from bs4 import BeautifulSoup
 
@@ -25,32 +26,6 @@ def _load_results_to_soup(filename):
 	with open('valid.html', 'rb') as f:
 		return BeautifulSoup(f.read())
 
-def post_html_to_validator(html_file):
-	url = 'http://validator.w3.org/check'
-
-	params = {
-		'uploaded_file': open(html_file).read(),
-		'charset':'(detect automatically)',
-		'doctype':'Inline',
-		'group':'0'
-	}
-
-	payload = urllib.urlencode(params)
-	req = urllib2.Request(url, data=payload)
-	return urllib2.urlopen(req)
-
-def is_valid(soup):
-	result = soup.find('h2', {'class':'valid'}) 
-	return True if result is not None else False
-
-def show_errors(soup):
-	error_loop = soup.find('ol', {'id':'error_loop'})
-	errors = [i.get_text().replace('\n', '').encode('ascii', 'ignore') 
-		for i in error_loop.find_all('li')]
-	
-	return [' '.join(i.split()) for i in errors]
-
-
 def get_file_list(files = None, directory=os.getcwd()):
 	if files is None: files = []
 	for i in os.listdir(directory):
@@ -61,6 +36,7 @@ def get_file_list(files = None, directory=os.getcwd()):
 			files.append(os.path.abspath(directory) + '\\' + i)
 	return files
 
+
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		file_list = sys.argv[1:]
@@ -68,22 +44,15 @@ if __name__ == '__main__':
 		file_list = get_file_list()
 
 	for f in file_list:
-		if 'html' not in f:
+		if 'html' not in f and 'css' not in f:
 			continue
 		filename = f.split('\\')[-1]
 		print 'Validating', filename
-		result = post_html_to_validator(f)
-		soup = BeautifulSoup(result.read())
+		if 'html' in filename.split('.')[-1].lower():
+			html.validate(f)
+		else:
+			css.validate(f)
 
-		if is_valid(soup):
-			print 'Passed all checks! No issues found with', filename
-			print 
-			continue 
-		# Errors found
-		print 'Errors found with file', filename
-		print 'File location:', f
-		for error in show_errors(soup):
-			print error
-		print 
-		time.sleep(.6)
 	user_wait = raw_input("Press any key to exit. ")
+	if user_wait == '\n':
+		sys.exit()
